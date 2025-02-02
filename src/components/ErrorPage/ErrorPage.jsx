38e5acf44da16be597efdefe5aba1f261e5942bd
mddalimmiah <mddalimsaif@gmail.com>
2025-02-03 01:16:23 +0600

    added card and format and setup all

---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+
+const ErrorPage = () => {
+    return (
+        <div className="text-center text-3xl">
+            <h2>Page Not Found</h2>
+            <p>status: 404</p>
+        </div>
+    );
+};
+
+export default ErrorPage;
